3f48dd76b5f8d39c80411ca7c69504bbb2ea6fb4
hzlilang <hzlilang@corp.netease.com>
2017-05-02 21:24:28 +0800

    add modal

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -44,8 +44,18 @@
       ...mapMutations([
         'DEL_LIST',
       ]),
+      /* eslint-disabled */
       del() {
-        this.DEL_LIST(this.index);
+        this.$alert('这是一段内容', '标题名称', {
+          confirmButtonText: '确定',
+          callback: (action) => {
+            this.$message({
+              type: 'info',
+              message: `action: ${action}`,
+            });
+          },
+        });
+        // this.DEL_LIST(this.index);
       },
     },
   };
